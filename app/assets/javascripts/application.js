9097407afdf2a2c2de5fc72a06faa89689da0335
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-15 23:15:14 -0300

    add message alert

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,26 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
+//= require toastr
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){  
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+})
