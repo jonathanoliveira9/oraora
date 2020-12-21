2773b633ef1a3da4162fe4f7ac98762b6a865eaf
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-21 17:31:58 -0300

    configure server sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'dashboards#index'
   resources :users do
